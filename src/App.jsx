5300faa02c2bafe227f3d886aad2b1fbf2201f2a
Guilherme Garros <62261737+GuiGarros@users.noreply.github.com>
2023-10-07 12:16:50 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginScreen from "./pages/login/login"
 
-function App() {
+function App() {  
   return (
     <>
       <BrowserRouter>
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,18 +1,24 @@
 import './login.css'
 import { useState } from 'react'
 import { TextField } from '@mui/material'
-function Login() {
-  const [count, setCount] = useState(0)
+import web3 from "./../../web3"
 
+function Login() {
+  const [count, setCount] = useState(0);
   return (
     <>
+      <body>
         <div className='background'>
-            <div className='login'>
-                <div className='objeto'>
-                <TextField id="standard-basic" label="Standard" variant="standard" />
+              <div className='titulo-on-top'>
+                <h1 className='titulo'>BUY & BUY</h1>
+              </div>
+                <div className='login'>
+                    <div className='objeto'>
+                    <TextField id="standard-basic" label="Standard" variant="standard" />
+                    </div>
                 </div>
-            </div>
         </div>
+      </body>
     </>
   )
 }
